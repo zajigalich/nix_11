8828881dddcb6959d6aaca931438182b884595a8
Konstantin <titarenko42k@gmail.com>
2022-02-18 18:43:19 +0200

    add hw_2(user_friendly_main)

--- hw_2_base_typess/src/main/java/ua/com/alevel/Main.java
+++ hw_2_base_typess/src/main/java/ua/com/alevel/Main.java
@@ -4,47 +4,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InputMismatchException {
-        programInterface();
-    }
+        public static void main(String[] args) throws InputMismatchException {
+            Scanner scanner = new Scanner(System.in);
+            int number;
 
-    private static void programInterface() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 1 to execute the program or 0 to exit");
-        String enterProgram = scanner.nextLine();
-        switch (enterProgram){
-            case "0" -> System.exit(0);
-            case "1" -> {
-                System.out.println("you forgot word conservative");
-                logic();
+            try{
+                do{
+                    System.out.println("Chose task(1-3; 0 - exit)");
+                    number = Integer.parseInt(scanner.nextLine());
+                    switch (number){
+                        case 1 -> new Numbers().output();
+                        case 2 -> new Symbols().output();
+                        case 3 -> new Lessons().output();
+                        case 0 -> System.exit(0);
+                        default -> System.out.println("There is no such choice");
+                    }
+                    System.out.println("\n");
+                }while (true);
+            }catch (NumberFormatException e){
+                System.out.println("Wrong button");
             }
         }
     }
-
-    private static void logic()  {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number of task(1-3)");
-        String executor = scanner.nextLine();
-        switch (executor) {
-            case "1" -> {
-                Numbers numb = new Numbers();
-                numb.output();
-                programInterface();
-            }
-            case "2" -> {
-                Symbols symbol = new Symbols();
-                symbol.output();
-                programInterface();
-            }
-            case "3" -> {
-                Lessons lessons = new Lessons();
-                lessons.output();
-            }
-            default -> {
-                System.out.println("Wrong button/if teaching is your benefactor");
-                logic();
-            }
-        }
-    }
-}
 
